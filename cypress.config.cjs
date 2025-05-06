9eddef7525445fca051f3081fab29650a0b21791
masa.sai <masa.1985@icloud.com>
2025-05-06 18:40:05 +0900

    E2EテストCI実行

---
+++ cypress.config.cjs
@@ -0,0 +1,10 @@
+// cypress.config.cjs
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:5173',
+    supportFile: false,
+    specPattern: 'cypress/e2e/**/*.cy.{js,ts}',
+  },
+})
